0ead881c47f8eecc33e6c0e44f385118ef5fbb90
Jakub <jakubwcislo18@gmail.com>
2023-08-31 15:34:45 +0200

    dodano wzory

--- kalkulator.py
+++ kalkulator.py
@@ -1,10 +1,10 @@
-inflacja = [1.592824484, 
+inflacja = [1.592824484,
 -0.453509101,
- 2.324671717,
- 1.261254407, 
-1.782526286, 
-2.329384541, 
-1.502229842, 
+2.324671717,
+1.261254407,
+1.782526286,
+2.329384541,
+1.502229842,
 1.782526286,
 2.328848994,
 0.616921348,
@@ -22,4 +22,19 @@ inflacja = [1.592824484,
 1.165733399,
 -0.404186718,
 1.499708521,
-]
+] 
+
+
+
+kredyt = int(input("Podaj wysokosc kredytu: "))
+opr = int(input("Podaj oprocentowanie w %: ")) #oprocentowanie
+rata = int(input("Podaj wysokosc raty: "))
+
+odsetki = kredyt*(opr/100)*31/365
+
+for i in inflacja:
+    kredyt = kredyt*(1+(i/100))
+    if kredyt<0:
+        kredyt*=-1
+    kredyt = kredyt - rata + odsetki
+    print(kredyt)
