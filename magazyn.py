9a2170d1c0206a3549f220baab33bab1ef9ac3c0
Jakub <jakubwcislo18@gmail.com>
2023-10-01 20:08:03 +0200

    pliki

--- magazyn.py
+++ magazyn.py
@@ -1,6 +1,74 @@
-balance = 0
-warehouse = {}
-history = []
+import os
+
+
+def save_money(balance):
+    file_path = "balance.txt"
+    with open(file_path, "w") as file:
+        file.write(f"{balance}")
+
+
+def get_money():
+    file_path = "balance.txt"
+    if not os.path.exists(file_path):
+        balance = 0
+    else:
+        with open(file_path, "r") as file:
+            balance = float(file.read())
+    return balance
+
+
+def save_storage(warehouse):
+    file_path = "warehouse.txt"
+    with open(file_path, "w") as file:
+        for item, data in warehouse.items():
+            price = data["price"]
+            amount = data["amount"]
+            file.write(f"{item}: {price}, {amount}\n")
+
+
+def get_storage():
+    file_path = "warehouse.txt"
+    warehouse = {}
+    if not os.path.exists(file_path):
+            with open(file_path, 'w'):
+                pass
+    else:
+        with open(file_path, "r") as file:
+            for line in file:
+                parts = line.strip().split(": ")
+                item = parts[0]
+                data_parts = parts[1].split(", ")
+                price = data_parts[0]
+                amount = data_parts[1]
+                warehouse[item] = {"price": price, "amount": amount}
+
+    return warehouse
+
+
+def save_history(history):
+    file_path = "history.txt"
+    with open(file_path, "w") as file:
+        for line in history:
+            file.write(f"{line}\n")
+
+
+def get_history():
+    history = []
+    file_path = "history.txt"
+    if not os.path.exists(file_path):
+        with open(file_path, 'w'):
+            pass
+    else:
+        with open(file_path, 'r') as file:
+            for line in file.readlines():
+                history.append(line.strip())
+    return history
+
+
+balance = get_money()
+history = get_history()
+warehouse = get_storage()
+
 
 while True:
     print("Wybierz numer")
@@ -13,9 +81,8 @@ while True:
     print("7. Przeglad")
     print("8. Koniec")
     number = input("Wybierz numer: ")
-    
-    
-    if number == "1": # SALDO
+
+    if number == "1":  # SALDO
         print("1. Wplata pieniedzy")
         print("2. Wyplata pieniedzy")
         print("3. Powrot do menu")
@@ -29,7 +96,7 @@ while True:
             history.append(f"Do konta dodano {change_balance}")
         elif number_in == "2":
             change_balance = int(input("Ile chcesz wyplacic?: "))
-            
+
             if balance - change_balance < 0:
                 print("Saldo nie moze byc ujemne!")
             else:
@@ -40,45 +107,42 @@ while True:
         else:
             print("Niewlasciwy numer!")
             continue
-            
-    
-    elif number == "2": # SPRZEDARZ
+
+    elif number == "2":  # SPRZEDARZ
         item = input("Podaj nazwe produktu: ")
         if item in warehouse:
             price = float(input("Podaj cenę produktu: "))
             amount = int(input("Podaj liczbe sztuk: "))
             if warehouse[item]["amount"] > amount:
                 warehouse[item]["amount"] -= amount
-                balance += price*amount
+                balance += price * amount
                 history.append(f"Sprzedaz: {item} sztuk {amount} za {price}")
             elif warehouse[item]["amount"] == amount:
                 del warehouse[item]
-                balance += price*amount
+                balance += price * amount
                 history.append(f"Sprzedaz: {item} sztuk {amount} za {price}")
             else:
                 print("Za malo w magazynie")
         else:
             print("Brak w magazynie")
 
-    
-    elif number == "3": # ZAKUP
+    elif number == "3":  # ZAKUP
         item = input("Podaj nazwe produktu?: ")
         price = float(input("Podaj cene produktu?: "))
         amount = int(input("Podaj liczbe sztuk: "))
-        
-        if balance - (price*amount) >= 0:
+
+        if balance - (price * amount) >= 0:
             if item in warehouse:
                 warehouse[item]["amount"] += amount
             else:
                 warehouse[item] = {"price": price, "amount": amount}
-            balance -= price*amount
+            balance -= price * amount
             history.append(f"Kupiono {item} za {price} w ilosci {amount}")
 
-    elif number == "4": # KONTO
+    elif number == "4":  # KONTO
         print(f"Stan konta to {balance}")
 
-        
-    elif number == "5": # LISTA
+    elif number == "5":  # LISTA
         if warehouse:
             for item, data in warehouse.items():
                 price = data["price"]
@@ -87,7 +151,7 @@ while True:
         else:
             print("Magazyn jest pusty")
 
-    elif number == "6": # MAGAZYN
+    elif number == "6":  # MAGAZYN
         item = input("Czego szukasz?: ")
         if item in warehouse:
             amount_in = warehouse[item]["amount"]
@@ -95,12 +159,11 @@ while True:
         else:
             print("Brak w magazynie")
 
-
-    elif number == "7": # PRZEGLAD
+    elif number == "7":  # PRZEGLAD
         print(f"Przeprowadzono {len(history)} operacji. Wybierz zakres.")
         start = input("Od: ")
         end = input("Do: ")
-        
+
         if not start:
             start = 0
         else:
@@ -122,17 +185,18 @@ while True:
         if start < 0 or end > len(history) or start >= end:
             print("Błędny zakres operacji.")
         elif start > 0:
-            for i in range(start-1, end):
+            for i in range(start - 1, end):
                 print(history[i])
         else:
             for i in range(start, end):
                 print(history[i])
 
-                
-    elif number == "8": # KONIEC
+    elif number == "8":  # KONIEC
+        save_storage(warehouse)
+        save_money(balance)
+        save_history(history)
         break
-    
-    
-    else: ######
+
+    else:  ######
         print("Niewlasciwy wybor")
         continue
